c3eeb5dbb21cc2f21f592393a8e83aea33c1fccd
Wisdom Kara <wiskara1@gmail.com>
2025-04-25 08:04:31 +0100

    tailwind and alll i needed has beeen insatlwd

---
+++ kaneltechnologies/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './public/index.html', // if you're using a standard public folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
