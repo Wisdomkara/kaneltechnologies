07bd5291c61a960b4ac06db24b2629267bb4e4f4
Wisdom Kara <wiskara1@gmail.com>
2025-05-06 10:28:54 +0100

    trying

--- kaneltechnologies/vite.config.js
+++ kaneltechnologies/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/kaneltechnologies/', // ✅ no angle brackets
+  base: '/kaneltechnologies/', 
   plugins: [react()],
 });
